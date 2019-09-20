bc503ae7c0310dcc76a5982bf50625ff956d11a0
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-09-20 13:30:29 +0300

    Add navbar component

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Navbar">
+    <button class="hamburger" type="button">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style>
+@import url("./navbar.css");
+</style>
