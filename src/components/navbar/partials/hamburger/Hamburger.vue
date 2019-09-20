48164da3cdb72b9346219a4ca0d93cca4c304b8e
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-09-20 21:58:04 +0300

    Add hamburger partial

---
+++ src/components/navbar/partials/hamburger/Hamburger.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="hamburger hamburger--squeeze"
+    v-bind:class="{ 'is-active': open }"
+    v-on:click="$emit('toggleNavbar')"
+    type="button"
+  >
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Hamburger",
+  props: {
+    open: Boolean
+  }
+};
+</script>
+
+<style>
+@import url("./hamburger.css");
+</style>
