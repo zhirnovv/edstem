abd56fe56357522a08605d0988f345260a21631b
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-09-20 21:57:47 +0300

    Add navbar component

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="Navbar">
-    <button class="hamburger" type="button">
-      <span class="hamburger-box">
-        <span class="hamburger-inner"></span>
-      </span>
-    </button>
-  </div>
+  <nav class="Navbar__container">
+    <Dropdown v-bind:open="navbarOpen"></Dropdown>
+    <div class="Navbar" v-bind:class="{ Navbar_open: navbarOpen }">
+      <Hamburger
+        v-bind:open="navbarOpen"
+        v-on:toggleNavbar="toggle('navbarOpen')"
+      ></Hamburger>
+      <div class="Navbar__brand">{{ brand }}</div>
+      <SearchButton
+        v-bind:open="searchbarOpen"
+        v-on:toggleSearchbar="toggleSearchbar"
+      ></SearchButton>
+    </div>
+  </nav>
 </template>
 
 <script>
+import Dropdown from "./partials/dropdown/Dropdown";
+import Hamburger from "./partials/hamburger/Hamburger";
+import SearchButton from "./partials/searchButton/SearchButton";
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  components: {
+    Dropdown,
+    Hamburger,
+    SearchButton
+  },
+  props: {
+    brand: String
+  },
+  data: () => ({
+    navbarOpen: false,
+    searchbarOpen: false
+  }),
+  methods: {
+    toggle(state) {
+      this[state] = !this[state];
+    },
+    toggleSearchbar() {
+      if (this.navbarOpen) {
+        this.toggle("navbarOpen");
+      }
+      this.toggle("searchbarOpen");
+    }
+  }
 };
 </script>
 
