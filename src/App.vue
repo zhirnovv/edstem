f24a0e45298f96cd62d85031340a2b1453bfe057
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-09-20 13:29:13 +0300

    Format files according to eslint

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <h1>HELLO YES ITS ME VUE</h1>
+  <Navbar></Navbar>
+  <!-- <h1>HELLO YES ITS ME VUE</h1> -->
 </template>
 
 <script>
+import Navbar from "./components/navbar/Navbar";
 export default {
-  name: 'app',
-}
+  name: "app",
+  components: {
+    Navbar
+  }
+};
 </script>
 
 <style>
-@import url('./assets/css/reset.css');
-@import url('./assets/css/main.css');
+@import url("./assets/css/reset.css");
+@import url("./assets/css/main.css");
 </style>
