80e7cb6221f4d7e3a9b2b99ece962b0f0f4c4a50
Vasily Zhirnov <vass.zhirnov@gmail.com>
2019-09-23 18:38:56 +0300

    Create Vue directive for swipe actions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from "vue";
+import Hammer from "hammerjs";
 import Vue2TouchEvents from "vue2-touch-events";
 import App from "./App.vue";
 
 Vue.use(Vue2TouchEvents);
+
+Vue.directive("pan", {
+  bind: function(el, binding) {
+    if (typeof binding.value === "function") {
+      const mc = new Hammer(el);
+      mc.get("pan").set({
+        direction: Hammer.DIRECTION_HORIZONTAL,
+        threshold: 20
+      });
+      mc.on("pan", binding.value);
+    }
+  }
+});
 
 Vue.config.productionTip = false;
 
